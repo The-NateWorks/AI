3a7ec2b45eb509ae5a8c65030d193fe1f7c2a904
The-NateWorks <lesternate6@outlook.com>
2026-04-29 19:57:07 -0500

    commit

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const http = require("http")
 const express = require("express")
 const {Server} = require("socket.io")
 const { exec } = require("child_process")
+const { parseArgs } = require("util")
+const { createClient } = require("@supabase/supabase-js");
 
 var app = express()
 var server = http.createServer(app)
 var io = new Server(server)
 
 process.stdin.setEncoding("utf-8");
+var client = createClient("https://jgjdxlulszliepzrhgff.supabase.co", "sb_publishable_JUr3sNI84pNyl30pzxD-dA_bvZhy1w3");
 
 var intervals = {}
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/admin", (req, res) => {
+    res.sendFile(path.resolve("./admin.html"))
 });
 
 io.on("connection", (socket) => {
@@ -46,24 +53,46 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    setInterval(async () => {
+        let data = await client.from("events").select("*").eq("program", "nautical");
+        data.data.forEach(async dati => {
+            for (c in dati.data.server.commands) {
+                let split = dati.data.server.commands[c];
+                dati.data.server.commands.splice(c, 1);
+                await client.from("events").update({data: dati.data}).eq("program", "nautical");
+                cmds(split[0], split.slice(1));
+            }
+        });
+    }, 500);
+});
 
 process.stdin.on("data", (data) => {
     let key = data.trim();
-    switch(key) {
+    let cmd = key.split(" ")[0];
+    let args = key.split(" ").slice(1);
+    cmds(cmd, args);
+});
+
+function cmds(cmd, args) {
+    switch(cmd) {
         case 'exit':
             process.exit();
         case 'sockets':
             console.log(Array.from(io.sockets.sockets.keys()).join("\n"));
             break;
+        case 'alertAll':
+            if (args[0]) {
+                io.emit("alert", args[0]);
+            }
 
         case '':
             break;
         default:
-            console.log(key + " is not a command.");
+            console.log(cmd + " is not a command.");
             break;
     }
-});
+}
 
 function parseJSON(string) {
     let levels = 1;
